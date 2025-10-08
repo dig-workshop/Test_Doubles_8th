c161af2d2d675a8d22796c2fcae834fa6ac43447
yukoTake <yuko_murase_aa@mail.toyota.co.jp>
2025-10-08 21:39:58 +0900

    警告修正

--- src/testDoubles/Dummy/RocketLauncher.test.ts
+++ src/testDoubles/Dummy/RocketLauncher.test.ts
@@ -1,6 +1,5 @@
 import {DummyRocket} from './DummyRocket'
 import {RocketLauncherImpl} from './RocketLauncher'
-import exp from "node:constants";
 import {StubFailureAuth} from "./StubAuth";
 
 describe('RocketLauncherImpl（ロケット発射装置）の認証機能のテスト', () => {
